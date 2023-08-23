93e439ed107e0065e9baea74a74d4095a327b5f6
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2023-08-23 16:56:53 +0100

    added cyrillic to the font

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,12 +1,13 @@
 import './globals.scss'
 import { Raleway } from 'next/font/google'
 import Navbar from './components/Navbar'
+
 export const metadata = {
   title: 'Coffee and Happiness',
   description: 'Coffe and Happiness website',
 }
 
-const raleway = Raleway({subsets: ['latin']})
+const raleway = Raleway({subsets: ['latin', 'cyrillic']})
 
 export default function RootLayout({ children }) {
   return (
