594010cff8cdf79451a102cabf513b74209be4de
hurrr1cane <maximgorak2005@gmail.com>
2023-08-23 19:12:41 +0300

    Added dark and light mode support

--- android/app/src/main/java/com/mdn/coffeeandhappiness/MainActivity.kt
+++ android/app/src/main/java/com/mdn/coffeeandhappiness/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.mdn.coffeeandhappiness
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.mdn.coffeeandhappiness.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         replaceFragment(HomeFragment())
 
         binding.bottomNavigationView.selectedItemId = R.id.nav_home
+
 
 
         binding.bottomNavigationView.setOnItemSelectedListener {
@@ -30,6 +38,40 @@ class MainActivity : AppCompatActivity() {
 
             }
             true
+        }
+
+        /**
+         * Settings
+         */
+        var sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+
+        /**
+         * Setting the mode
+         */
+        controllerLightDarkMode(sharedPreferences)
+
+
+    }
+
+    private fun controllerLightDarkMode(sharedPreferences: SharedPreferences) {
+        val nightMode: Boolean = sharedPreferences.getBoolean("Night", true)
+        val modeButton = findViewById<ImageButton>(R.id.modeButton)
+        if (nightMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            modeButton.setImageResource(R.drawable.darkmode_icon)
+        }
+        modeButton.setOnClickListener() {
+            val editor = sharedPreferences.edit()
+            if (nightMode) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                modeButton.setImageResource(R.drawable.lightmode_icon)
+                editor.putBoolean("Night", false)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                modeButton.setImageResource(R.drawable.darkmode_icon)
+                editor.putBoolean("Night", true)
+            }
+            editor.apply()
         }
     }
 
