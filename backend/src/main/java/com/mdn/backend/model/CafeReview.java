e83cc2350289a10a2b07ae3cc34c01c3a3748038
d3nnyyy <d3nnn41k@gmail.com>
2023-08-23 23:00:26 +0300

    created two Entities - CafeReview and FoodReview which are subclasses of Review

---
+++ backend/src/main/java/com/mdn/backend/model/CafeReview.java
@@ -0,0 +1,21 @@
+package com.mdn.backend.model;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CafeReview extends Review {
+
+    @ManyToOne
+    @JoinColumn(name = "cafe_id")
+    private Cafe cafe;
+
+}
---
+++ backend/src/main/java/com/mdn/backend/model/FoodReview.java
@@ -0,0 +1,22 @@
+package com.mdn.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FoodReview extends Review {
+
+    @ManyToOne
+    @JoinColumn(name = "food_id")
+    private Food food;
+
+}
