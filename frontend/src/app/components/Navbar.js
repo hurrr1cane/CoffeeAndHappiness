d71fd96690a14c5aa806cc871e58d7aa6b77e69f
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2023-08-23 16:55:42 +0100

    scaffolded basic app structure, added scss

---
+++ frontend/src/app/components/Navbar.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+import styles from "./Navbar.module.scss"
+
+export default function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+            <Link className={styles.link} href="/"><h1>Home</h1></Link>
+            <Link className={styles.link} href="/institutions"><h1>Institutions</h1></Link>
+            <Link className={styles.link} href="/menu"><h1>Menu</h1></Link>
+            <Link className={styles.link} href="/about-us"><h1>About us</h1> </Link>
+        </nav>
+    )
+}
---
+++ frontend/src/app/institutions/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Institutions page</h1>
+        </div>
+    )
+}
