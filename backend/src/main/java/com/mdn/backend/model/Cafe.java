db64beafd1f766e85e1628ff6704b6ba671953e6
d3nnyyy <d3nnn41k@gmail.com>
2023-08-23 20:18:23 +0300

    added validations for fields

--- backend/src/main/java/com/mdn/backend/model/Cafe.java
+++ backend/src/main/java/com/mdn/backend/model/Cafe.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -16,11 +20,17 @@ public class Cafe {
     private Integer id;
 
     @Column(name = "location")
+    @NotBlank(message = "Location is required")
+    @Size(max = 255, message = "Location cannot exceed 255 characters")
     private String location;
 
     @Column(name = "image_url")
+    @Pattern(regexp = "^https?://.*",
+            message = "Image URL must start with http:// or https://")
     private String imageUrl;
 
     @Column(name = "phone_number")
+    @Pattern(regexp = "(?:\\+380|0)\\d{9}",
+            message = "Phone number must be in format +380XXXXXXXXX or 0XXXXXXXXX")
     private String phoneNumber;
 }
