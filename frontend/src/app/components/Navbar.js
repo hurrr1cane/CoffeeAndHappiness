dbddd710f7ee84dadb9f3dda1876e5f928816f55
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2023-08-24 22:27:35 +0100

    changed navbar styling

--- frontend/src/app/components/Navbar.js
+++ frontend/src/app/components/Navbar.js
@@ -12,33 +12,37 @@
     const pathname = usePathname();
     const isWindow = typeof window === undefined
     const [selectedTab, setSelectedTab] = useState(isWindow ? localStorage.getItem("selectedTab") ?? "home" : "home");
-
-  useEffect(() => {
-    const storedTab = localStorage.getItem("selectedTab");
-    if (storedTab) {
-      setSelectedTab(storedTab);
-    }
-  }, []);
+    const [markerLeft, setMarkerLeft] = useState(0);
+    const [markerRight, setMarkerRight] = useState(0) 
+    useEffect(() => {
+      const storedTab = localStorage.getItem("selectedTab");
+      if (storedTab) {
+        setSelectedTab(storedTab);
+      }
+    }, []);
 
   useEffect(() => {
     const routeName = pathname.replace("/", "");
     setSelectedTab(routeName || "home");
   }, [pathname]);
 
-  const handleTabClick = (tab) => {
+  const handleTabClick = (e, tab) => {
     setSelectedTab(tab);
     localStorage.setItem("selectedTab", tab); 
+    setMarkerLeft(e.target.offsetLeft)
+    setMarkerRight(e.target.offsetRight)
   };
     return (
       <nav className={styles.navbar}>
-        <Link onClick={() => {handleTabClick('home')}} key={'home'} className={`${styles.link} ${selectedTab === 'home' ? styles.selected : '' }`} href="/"><h1>Home</h1></Link>
-        <Link onClick={() => {handleTabClick('institutions')}} key={'institutions'} className={`${styles.link} ${selectedTab === 'institutions' ? styles.selected : '' }`} href="/institutions"><h1>Institutions</h1></Link>
-        <Link onClick={() => {handleTabClick('menu')}} key={'menu'} className={`${styles.link} ${selectedTab === 'menu' ? styles.selected : '' }`} href="/menu"><h1>Menu</h1></Link>
-        <Link onClick={() => {handleTabClick('about-us')}} key={'about-us'} className={`${styles.link} ${selectedTab === 'about-us' ? styles.selected : '' }`} href="/about-us"><h1>About us</h1></Link>
-        <Link onClick={() => {handleTabClick('user')}} key={'user'} className={`${styles.link} ${selectedTab === 'user' ? styles.selected : '' }`} href="/user"><h1>User</h1></Link>
+        <div className={styles.marker} style={{left:markerLeft, right: markerRight}}></div>
+        <Link onClick={(e) => {handleTabClick(e, 'home')}} key={'home'} className={`${styles.link} ${selectedTab === 'home' ? styles.selected : '' }`} href="/"><h1>Home</h1></Link>
+        <Link onClick={(e) => {handleTabClick(e, 'institutions')}} key={'institutions'} className={`${styles.link} ${selectedTab === 'institutions' ? styles.selected : '' }`} href="/institutions"><h1>Institutions</h1></Link>
+        <Link onClick={(e) => {handleTabClick(e, 'menu')}} key={'menu'} className={`${styles.link} ${selectedTab === 'menu' ? styles.selected : '' }`} href="/menu"><h1>Menu</h1></Link>
+        <Link onClick={(e) => {handleTabClick(e, 'about-us')}} key={'about-us'} className={`${styles.link} ${selectedTab === 'about-us' ? styles.selected : '' }`} href="/about-us"><h1>About us</h1></Link>
+        <Link onClick={(e) => {handleTabClick(e, 'user')}} key={'user'} className={`${styles.link} ${selectedTab === 'user' ? styles.selected : '' }`} href="/user"><h1>User</h1></Link>
         <div className={styles['icons-container']}>
           <Brightness2Icon/>
-          <Image className={styles.image} src={EnglishImage}/>
+          <Image className={styles.image} src={EnglishImage} alt="image of the flag of United Kingdom"/>
         </div>
       </nav>
     );
