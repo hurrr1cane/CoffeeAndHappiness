5637eee11bf0d10009c4036948fd8930568309b5
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2023-08-25 15:33:05 +0100

    scaffolded out review structure

--- frontend/src/app/menu/dish/[id]/Dish.js
+++ frontend/src/app/menu/dish/[id]/Dish.js
@@ -9,6 +9,14 @@ export default function Dish() {
     const pathname = usePathname().split('dish/')[1]
 
     return (
-        <h2>{pathname}</h2>
+        <main>
+            <section className={styles.section}>
+                <div className={styles.image}> Image placeholder, image id: {pathname}</div>
+                <p>Image description: <br/> 
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Ad aliquam officiis error dolor voluptate repellat, ullam non suscipit neque recusandae 
+                cupiditate deleniti aliquid quibusdam incidunt deserunt optio facere sequi magni?</p>
+            </section>
+        </main>
     )
 }
--- frontend/src/app/menu/dish/[id]/page.js
+++ frontend/src/app/menu/dish/[id]/page.js
@@ -1,11 +1,13 @@
 import styles from './page.module.scss'
 import Dish from './dish'
+import Review from './Review'
 export default function Home() {
     return (
-    <main className={styles.main}>
+    <div className={styles.main}>
         <h1>Dish page</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati commodi corporis soluta nisi aliquam pariatur hic, illum voluptate omnis mollitia, quas, natus at! Similique dolores accusantium laborum minima repellat! Officiis!</p>
         <Dish/>
-     </main>
+        <Review/>
+     </div> 
     )
 }
