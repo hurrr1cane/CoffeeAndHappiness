183dc686efef662b3e31c9bf63b90cab4b3e55e3
d3nnyyy <d3nnn41k@gmail.com>
2023-08-25 09:51:08 +0300

    created news entity

---
+++ backend/src/main/java/com/mdn/backend/model/News.java
@@ -0,0 +1,36 @@
+package com.mdn.backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title")
+    @NotNull(message = "Title is required")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "url")
+    @Pattern(regexp = "^https?://.*",
+            message = "Image URL must start with http:// or https://")
+    private String imageUrl;
+
+    @Column(name = "published_at")
+    @NotNull(message = "Published at is required")
+    private String publishedAt;
+
+}
