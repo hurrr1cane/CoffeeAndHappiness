441d205cb8f9774d76e63d1644710bf1460f3bc9
d3nnyyy <d3nnn41k@gmail.com>
2023-08-25 23:28:37 +0300

    added javadoc to JwtService

--- backend/src/main/java/com/mdn/backend/security/JwtService.java
+++ backend/src/main/java/com/mdn/backend/security/JwtService.java
@@ -13,24 +13,54 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.function.Function;
 
+/**
+ * Service class responsible for JWT token generation and validation.
+ */
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "1fe9fd10ac9e24c0d45e727c4b79569c593a508197cf3a8f83feb31f2ee9d8aa";
 
+    /**
+     * Extracts the email from a JWT token.
+     *
+     * @param token The JWT token.
+     * @return The email extracted from the token.
+     */
     public String extractEmail(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
+    /**
+     * Extracts a claim from a JWT token.
+     *
+     * @param token          The JWT token.
+     * @param claimsResolver The claims resolver function.
+     * @param <T>            The type of the claim.
+     * @return The extracted claim.
+     */
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
+    /**
+     * Generates a JWT token for the given user details and additional claims.
+     *
+     * @param userDetails The user details.
+     * @return The generated JWT token.
+     */
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
+    /**
+     * Generates a JWT token with specified claims for the given user details.
+     *
+     * @param claims      Additional claims to include in the token.
+     * @param userDetails The user details.
+     * @return The generated JWT token.
+     */
     public String generateToken(HashMap<String, Object> claims, UserDetails userDetails) {
         return Jwts
                 .builder()
@@ -43,19 +73,44 @@ public class JwtService {
 
     }
 
+    /**
+     * Validates a JWT token for the given user details.
+     *
+     * @param token       The JWT token.
+     * @param userDetails The user details.
+     * @return True if the token is valid, false otherwise.
+     */
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String email = extractEmail(token);
         return (email.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
+    /**
+     * Checks if a JWT token is expired.
+     *
+     * @param token The JWT token.
+     * @return True if the token is expired, false otherwise.
+     */
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
+    /**
+     * Extracts the expiration date from a JWT token.
+     *
+     * @param token The JWT token.
+     * @return The expiration date.
+     */
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
+    /**
+     * Extracts all claims from a JWT token.
+     *
+     * @param token The JWT token.
+     * @return The claims extracted from the token.
+     */
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
@@ -65,6 +120,11 @@ public class JwtService {
                 .getBody();
     }
 
+    /**
+     * Gets the signing key used for JWT token verification.
+     *
+     * @return The signing key.
+     */
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
