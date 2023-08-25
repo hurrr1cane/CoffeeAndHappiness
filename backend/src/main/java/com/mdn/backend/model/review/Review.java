17fdf7f24d9a8678a18f87f9466b48c0d5ae6537
d3nnyyy <d3nnn41k@gmail.com>
2023-08-25 23:18:32 +0300

    added second language to Review

--- backend/src/main/java/com/mdn/backend/model/review/Review.java
+++ backend/src/main/java/com/mdn/backend/model/review/Review.java
@@ -22,6 +22,10 @@ public abstract class Review {
     @Min(value = 1, message = "Rating must be at least 1")
     @Max(value = 5, message = "Rating must be at most 5")
     private int rating;
-    private String comment;
 
+    @Column(name = "comment_en")
+    private String commentEN;
+
+    @Column(name = "comment_ua")
+    private String commentUA;
 }
