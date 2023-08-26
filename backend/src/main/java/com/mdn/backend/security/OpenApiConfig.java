366e8df1e6a075a39d2f09fe1b0d4e8eb380f98c
d3nnyyy <d3nnn41k@gmail.com>
2023-08-26 13:10:25 +0300

    added swagger configuration

---
+++ backend/src/main/java/com/mdn/backend/security/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.mdn.backend.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        description = "JWT token",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
