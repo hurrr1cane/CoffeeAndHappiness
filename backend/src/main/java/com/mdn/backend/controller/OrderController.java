09d5b19ab5d1e969728236f5ac1faa069756cf47
d3nnyyy <d3nnn41k@gmail.com>
2023-08-26 13:25:14 +0300

    customized order endpoints for better understanding requests

--- backend/src/main/java/com/mdn/backend/controller/OrderController.java
+++ backend/src/main/java/com/mdn/backend/controller/OrderController.java
@@ -6,6 +6,12 @@ import com.mdn.backend.model.order.Order;
 import com.mdn.backend.model.order.OrderRequest;
 import com.mdn.backend.service.FoodService;
 import com.mdn.backend.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +30,11 @@ public class OrderController {
     private final OrderService orderService;
     private final FoodService foodService;
 
+    @Operation(summary = "Place a new order", description = "Place a new order for a user with a list of selected foods.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Order created", content = @Content(schema = @Schema(implementation = Order.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid food id", content = @Content(schema = @Schema(implementation = String.class), examples = {@ExampleObject(name = "Invalid food id", value = "One or more selected foods not found")}))
+    })
     @PostMapping
     public ResponseEntity<?> placeOrder(@RequestBody @Valid OrderRequest orderRequest) {
         log.info("Placing a new order");
