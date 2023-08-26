2fccc1a51b36a2fcf9199584ebb7ae44ac4d4443
d3nnyyy <d3nnn41k@gmail.com>
2023-08-26 13:10:15 +0300

    changed security config to allow seeing swagger ui

--- backend/src/main/java/com/mdn/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/mdn/backend/security/SecurityConfig.java
@@ -37,6 +37,20 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
+                .requestMatchers(
+                        "/api/v1/auth/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui/**",
+                        "/webjars/**",
+                        "/swagger-ui.html"
+                )
+                    .permitAll()
                 .requestMatchers("/api/auth/**")
                     .permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/**")
