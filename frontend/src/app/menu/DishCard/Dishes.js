d5861d11f780bd08b55b78daade116ddbcf5a657
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2023-08-27 20:57:09 +0100

    added menu item request

--- frontend/src/app/menu/DishCard/Dishes.js
+++ frontend/src/app/menu/DishCard/Dishes.js
@@ -2,74 +2,16 @@
 import dishPic from '../../../../public/pizza.jpg'
 import styles from './Dishes.module.scss'
 import DishCard from "./DishCard";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 export default function Dishes({ tab }) {
-    // useEffect(() => {
-    //     axios.get(`http://localhost:8080/api/food/type/${tab}`)
-    //     .then(res => console.log(res))
-    //     .catch(err => console.log(err))
-    // })
-    const data = [
-        {
-            name: "Pizza",
-            image: dishPic,
-            description: "Appetizing pizza with Italian dough",
-            rating: "4",
-            price: "₴135.73",
-            id: "1"
-        },
-        {
-            name: "Cappuccino",
-            image: dishPic,
-            description: "A rich and creamy espresso-based coffee topped with frothed milk.",
-            rating: "4.8",
-            price: "₴107.73",
-            id: "2"
-        },
-        {
-            name: "Croissant",
-            image: dishPic,
-            description: "Flaky and buttery French pastry that's perfect for breakfast.",
-            rating: "4.5",
-            price: "₴67.23",
-            id: "3"
-        },
-        {
-            name: "Avocado Toast",
-            image: dishPic,
-            description: "Sourdough toast topped with smashed avocado, red pepper flakes, and a poached egg.",
-            rating: "4.6",
-            price: "₴161.73",
-            id: "4"
-        },
-        {
-            name: "Chicken Panini",
-            image: dishPic,
-            description: "Grilled panini with seasoned chicken, melted cheese, and fresh vegetables.",
-            rating: "4.4",
-            price: "₴202.23",
-            id: "5"
-        },
-        {
-            name: "Mixed Berry Parfait",
-            image: dishPic,
-            description: "Layers of Greek yogurt, granola, and assorted fresh berries.",
-            rating: "4.7",
-            price: "₴134.73",
-            id: "6"
-        },
-        {
-            name: "Pesto Pasta",
-            image: dishPic,
-            description: "Penne pasta tossed in homemade basil pesto sauce and cherry tomatoes.",
-            rating: "4.3",
-            price: "₴242.73",
-            id: "7"
-        }
-    ];
-    
+    const [data, setData] = useState([])
 
+    useEffect(() => {
+        axios.get(`http://localhost:8080/api/food/type/${tab}`)
+        .then(res => setData(res.data))
+        .catch(err => console.log(err))
+    }, [tab])
 
     return (
         <div className={styles.container}>
