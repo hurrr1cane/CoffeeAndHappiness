6d472e32b05159e63596f425b4a8e046ac5a67b4
d3nnyyy <d3nnn41k@gmail.com>
2023-08-30 13:01:46 +0300

    created user edit request for editing profile

--- backend/src/main/java/com/mdn/backend/model/user/User.java
+++ backend/src/main/java/com/mdn/backend/model/user/User.java
@@ -14,11 +14,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @Data
---
+++ backend/src/main/java/com/mdn/backend/model/user/UserEditRequest.java
@@ -0,0 +1,16 @@
+package com.mdn.backend.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserEditRequest {
+
+    private String firstName;
+    private String lastName;
+    private String imageUrl;
+
+}
