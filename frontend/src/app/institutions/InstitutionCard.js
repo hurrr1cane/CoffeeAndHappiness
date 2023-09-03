e3a4e21f19ef3350bc8ae2f113e760c875efe719
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2023-09-03 18:48:21 +0100

    added more responsivness to the cafe page

--- frontend/src/app/institutions/InstitutionCard.js
+++ frontend/src/app/institutions/InstitutionCard.js
@@ -31,31 +31,44 @@ export default function InstitutionCard({ cafe }) {
     }
 
     return (
-    <div className={styles.card}>
-        {user.role === 'ADMIN' && <EditCafeModal cafe={cafe} open={open} setOpen={setOpen} id={cafe.id} token={user.token}/>}
-
-        <Image
-        src={cafe?.imageUrl}
-        className={styles.image}
-        alt={'image of a restaurant'}
-        height={200}
-        width={200}
-        />
-        <div className={styles.description}>
-            <p>{cafe?.locationEN}</p>
-            <p>Phone number: {cafe.phoneNumber}</p>
-            <p>
-                <Link href="https://www.google.com/maps">View on a map</Link>
+        <div className={styles.card}>
+        {user.role === 'ADMIN' && (
+          <EditModal
+            item={cafe}
+            open={open}
+            setOpen={setOpen}
+            id={cafe.id}
+            token={user.token}
+          />
+        )}
+      
+          <Image
+            src={cafe?.imageUrl}
+            className={styles.image}
+            alt={'image of a restaurant'}
+            height={200}
+            width={200}
+          />
+          <div>
+            <h1 className={styles.property}>{cafe?.locationEN}</h1>
+            <p className={styles.property}>Phone number: {cafe.phoneNumber}</p>
+            <p className={styles.property}>
+              <Link href="https://www.google.com/maps">View on a map</Link>
             </p>
-            <p>
-                <Link href={`institutions/institution/${cafe?.id}`}>View reviews</Link>
+            <p className={styles.property}>
+              <Link href={`institutions/institution/${cafe?.id}`}>View reviews</Link>
             </p>
-        </div>  
-        {user.role === 'ADMIN' ? 
-                    <Stack direction="row" sx={{marginLeft:"auto"}}>
-                        <Fab onClick={handleDeleteClick} sx={{bgcolor:"#FF0000", "&:hover":{bgcolor:"#FF0000"}, marginLeft:"auto", color:"white", marginRight:"1rem"}}><Close/></Fab>
-                        <Fab onClick={handleEditClick} sx={{bgcolor:"#4caf81", "&:hover":{bgcolor:"#4caf81"}, marginLeft:"auto", color:"white", marginRight:"1rem"}}><Edit/></Fab>
-                    </Stack> : ''}
-    </div>
+          </div>
+        {user.role === 'ADMIN' && (
+          <div className={styles.adminActions}>
+            <Fab onClick={handleDeleteClick} className={styles.fab}>
+              <Close />
+            </Fab>
+            <Fab onClick={handleEditClick} className={styles.fab}>
+              <Edit />
+            </Fab>
+          </div>
+        )}
+      </div>      
     )
 }
