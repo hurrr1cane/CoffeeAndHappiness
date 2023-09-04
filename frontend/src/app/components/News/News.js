e169fbc58f099ce3fcab28c613ea05ce4ca670d4
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2023-09-04 13:32:21 +0100

    made the news responsive

--- frontend/src/app/components/News/News.js
+++ frontend/src/app/components/News/News.js
@@ -4,9 +4,18 @@ import styles from './news.module.scss'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { Close } from '@mui/icons-material';
+import { Edit } from '@mui/icons-material';
+import {Stack, Fab} from '@mui/material'
+import { useGlobalContext } from '@/app/store/store';
+import Link from 'next/link';
 function News() {
 
     const [news, setNews] = useState([])
+
+    const {user, _} = useGlobalContext()
+
+    const [open, setOpen] = useState(false)
 
     useEffect(() => {
         axios.get("http://localhost:8080/api/news")
@@ -14,19 +23,53 @@ function News() {
         .catch(err => console.log(err))
     })
 
+
+    const handleDeleteClick = (id) => {
+        axios.delete(`http://localhost:8080/api/news/${id}`, {
+            headers: {
+                Authorization: "Bearer " + user.token
+            }
+        })
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
+ 
+
+    const handleEditClick = () => {
+        setOpen(true)
+    }
+
+
   return (
     <div className={styles.container}>
-        {news.map(newsItem =>(
-            <div key={newsItem.id} className={styles.card}>
-                <div className={styles.content}>
-                    <Image className={styles.image} src={"https://coffee-and-happiness-images.s3.eu-central-1.amazonaws.com/coffee.jpeg"} width={400} height={200} alt="image of some news"/>
-                    <h2>{newsItem.titleEN}</h2>
-                    <p>{newsItem.descriptionEN}</p>
-                </div>
+    <div className={styles.columnContainer}>
+      {news.map((newsItem, index) => (
+        <div key={newsItem.id} className={styles.card}>
+          {user.role === 'ADMIN' && <EditNewsModal item={newsItem} open={open} setOpen={setOpen} id={newsItem.id} token={user.token} />}
+          <div className={styles.link}>
+            <Image
+              className={styles.image}
+              src={"/pizza.jpg"}
+              height={200}
+              width={200}
+              alt='picture of a pizza'
+            />
+            <div>
+              <h1 className={styles.property}>{newsItem.titleEN}</h1>
+              <p className={styles.property}>{newsItem.descriptionEN}</p>
             </div>
-        ))}
+          </div>
+          {user.role === 'ADMIN' ? 
+            <Stack direction="row">
+              <Fab onClick={() => handleDeleteClick(newsItem.id)} sx={{bgcolor:"#FF0000", "&:hover":{bgcolor:"#FF0000"}, marginLeft:"auto", color:"white", marginRight:"1rem"}}><Close /></Fab>
+              <Fab onClick={handleEditClick} sx={{bgcolor:"#4caf81", "&:hover":{bgcolor:"#4caf81"}, marginLeft:"auto", color:"white", marginRight:"1rem"}}><Edit /></Fab>
+            </Stack> : ''}
+        </div>
+      ))}
     </div>
-  );
+  </div>
+);
 }
+
 
 export default News;
