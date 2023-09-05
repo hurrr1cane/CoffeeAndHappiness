e8db3fb42286ccb3e989b3a2488693025d010175
hurrr1cane <maximgorak2005@gmail.com>
2023-09-05 16:07:17 +0300

    Added switch back to map view

--- android/app/src/main/java/com/mdn/coffeeandhappiness/fragments/mapfragments/MapListFragment.kt
+++ android/app/src/main/java/com/mdn/coffeeandhappiness/fragments/mapfragments/MapListFragment.kt
@@ -5,15 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mdn.coffeeandhappiness.R
 import com.mdn.coffeeandhappiness.adapter.CafeRecyclerViewAdapter
-import com.mdn.coffeeandhappiness.adapter.FoodRecyclerViewAdapter
 import com.mdn.coffeeandhappiness.controller.CafeController
-import com.mdn.coffeeandhappiness.controller.FoodController
 import com.mdn.coffeeandhappiness.exception.NoInternetException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -47,6 +46,17 @@ class MapListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_map_list, container, false)
+
+        val changeToMap = view.findViewById<ImageButton>(R.id.mapChangeToMap)
+
+        changeToMap.setOnClickListener {
+            val mapFragment = MapMapFragment()
+            val fragmentManager = requireActivity().supportFragmentManager
+            val transaction = fragmentManager.beginTransaction()
+            transaction.replace(R.id.mapFrame, mapFragment)
+            transaction.addToBackStack(null) // Optional: Add to back stack for navigation
+            transaction.commit()
+        }
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.mapListRecyclerView)
 
