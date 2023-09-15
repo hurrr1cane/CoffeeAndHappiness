0af6df623e7495a6fb2acc3e88850a479c0c6f2b
d3nnyyy <d3nnn41k@gmail.com>
2023-09-15 18:22:03 +0300

    modified CafeService to use dto

--- backend/src/main/java/com/mdn/backend/model/Cafe.java
+++ backend/src/main/java/com/mdn/backend/model/Cafe.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +17,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Cafe {
 
     @Id
