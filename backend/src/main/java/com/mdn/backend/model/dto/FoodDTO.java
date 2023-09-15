db57423661dd99ed65185068b0c20f8554ab1786
d3nnyyy <d3nnn41k@gmail.com>
2023-09-15 18:28:44 +0300

    modified FoodService to use dto

--- backend/src/main/java/com/mdn/backend/model/dto/FoodDTO.java
+++ backend/src/main/java/com/mdn/backend/model/dto/FoodDTO.java
@@ -1,5 +1,6 @@
 package com.mdn.backend.model.dto;
 
+import com.mdn.backend.model.FoodType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,10 +16,13 @@ public class FoodDTO {
     private String descriptionEN;
     private String descriptionUA;
 
+    private String ingredientsEN;
+    private String ingredientsUA;
+
     private String imageUrl;
 
-    private double price;
-
-    private String foodType;
+    private Double weight;
+    private Double price;
+    private FoodType type;
 
 }
