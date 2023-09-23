59021fdf11caf91beab13722c6ebcd2d8054c2c8
d3nnyyy <d3nnn41k@gmail.com>
2023-09-23 21:32:54 +0300

    modified email service to also have sendEmailVerification method

--- backend/src/main/java/com/mdn/backend/auth/forgotpassword/EmailService.java
+++ backend/src/main/java/com/mdn/backend/auth/forgotpassword/EmailService.java
@@ -2,43 +2,51 @@ package com.mdn.backend.auth.forgotpassword;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
 
-    public void sendVerificationCode(String email, String verificationCode) {
-
+    private void sendEmail(String email, String subject, String messageContent) {
         try {
-
             MimeMessage message = javaMailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
             helper.setTo(email);
-            helper.setSubject("Verification Code");
-
-            String htmlContent = "<html><body>" +
-                    "<p>Hello,</p>" +
-                    "<p>Your verification code is: <strong>" + verificationCode + "</strong></p>" +
-                    "<p>Please use this code to reset your password.</p>" +
-                    "</body></html>";
-
-            helper.setText(htmlContent, true);
+            helper.setSubject(subject);
+            helper.setText(messageContent, true);
 
             javaMailSender.send(message);
-
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
+    public void sendVerificationCode(String email, String verificationCode) {
+        String subject = "Verification Code";
+        String htmlContent = generateEmailHtmlContent(verificationCode, "reset your password");
+        sendEmail(email, subject, htmlContent);
+    }
+
+    public void sendEmailVerification(String email, String verificationCode) {
+        String subject = "Email Verification";
+        String htmlContent = generateEmailHtmlContent(verificationCode, "verify your email");
+        sendEmail(email, subject, htmlContent);
+    }
+
+    private String generateEmailHtmlContent(String verificationCode, String actionDescription) {
+        return "<html><body>" +
+                "<p>Hello,</p>" +
+                "<p>Your verification code is: <strong>" + verificationCode + "</strong></p>" +
+                "<p>Please use this code to " + actionDescription + ".</p>" +
+                "</body></html>";
+    }
 }
