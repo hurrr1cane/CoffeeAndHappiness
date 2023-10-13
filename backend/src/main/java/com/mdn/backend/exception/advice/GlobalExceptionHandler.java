5bba555860f570032f44648174e2d3b4eb57939c
d3nnyyy <d3nnn41k@gmail.com>
2023-10-13 17:17:51 +0300

    enhanced global exception handler

--- backend/src/main/java/com/mdn/backend/exception/advice/GlobalExceptionHandler.java
+++ backend/src/main/java/com/mdn/backend/exception/advice/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
-package com.mdn.backend.advice;
+package com.mdn.backend.exception.advice;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,47 +10,30 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-import java.nio.file.AccessDeniedException;
+import java.security.SignatureException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    public static final String ERROR = "error";
+    public static final String MESSAGE = "message";
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
         Map<String, String> errorMap = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> {
-            errorMap.put(error.getField(), error.getDefaultMessage());
-        });
+        errorMap.put(ERROR, "Invalid Argument");
+        errorMap.put(MESSAGE, ex.getMessage());
         return errorMap;
     }
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(RuntimeException.class)
-    public Map<String, String> handleBusinessException(RuntimeException ex) {
+    @ExceptionHandler(SignatureException.class)
+    public Map<String, String> handleSignatureException(SignatureException ex) {
         Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("errorMessage", ex.getMessage());
-        return errorMap;
-    }
-
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler(InsufficientAuthenticationException.class)
-    public Map<String, String> handleInsufficientAuthentication(InsufficientAuthenticationException ex) {
-        Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("error", "Insufficient Authentication");
-        errorMap.put("message", ex.getMessage());
-        return errorMap;
-    }
-
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler(AuthenticationException.class)
-    public Map<String, String> handleAuthenticationException(AuthenticationException ex) {
-        Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("error", "Authentication Error");
-        errorMap.put("message", ex.getMessage());
+        errorMap.put(ERROR, "Invalid Token");
+        errorMap.put(MESSAGE, ex.getMessage());
         return errorMap;
     }
 
@@ -58,8 +41,26 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AccessDeniedException.class)
     public Map<String, String> handleAccessDeniedException(AccessDeniedException ex) {
         Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("error", "Access Denied");
-        errorMap.put("message", ex.getMessage());
+        errorMap.put(ERROR, "Access Denied");
+        errorMap.put(MESSAGE, ex.getMessage());
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    public Map<String, String> handleInsufficientAuthentication(InsufficientAuthenticationException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put(ERROR, "Insufficient Authentication");
+        errorMap.put(MESSAGE, ex.getMessage());
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(AuthenticationException.class)
+    public Map<String, String> handleAuthenticationException(AuthenticationException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put(ERROR, "Authentication Failure");
+        errorMap.put(MESSAGE, ex.getMessage());
         return errorMap;
     }
 
@@ -67,8 +68,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoHandlerFoundException.class)
     public Map<String, String> handleNoHandlerFoundException(NoHandlerFoundException ex) {
         Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("error", "Resource Not Found");
-        errorMap.put("message", ex.getMessage());
+        errorMap.put(ERROR, "Resource Not Found");
+        errorMap.put(MESSAGE, ex.getMessage());
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(RuntimeException.class)
+    public Map<String, String> handleBusinessException(RuntimeException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put(ERROR, "Business Exception");
+        errorMap.put(MESSAGE, ex.getMessage());
         return errorMap;
     }
 
@@ -76,8 +86,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public Map<String, String> handleGenericException(Exception ex) {
         Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("error", "Internal Server Error");
-        errorMap.put("message", ex.getMessage());
+        errorMap.put(ERROR, "Internal Server Error");
+        errorMap.put(MESSAGE, ex.getMessage());
         return errorMap;
     }
 
